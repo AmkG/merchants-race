1f9a1f2d391a27f6df6a07e1e0c53184be1e183d
Alan Manuel K. Gloria <almkglor@gmail.com>
2013-11-12 22:07:27 +0800

    Merch.Race.Hex: Hex coordinates handling.

---
+++ Merch/Race/Hex.hs
@@ -0,0 +1,117 @@
+{- Hex coordinate system.
+
+The hex coordinate system is the "axial" coordinate
+system of Amit Patel, from:
+   http://www.redblobgames.com/grids/hexagons/#coordinates
+
+We are using "flat-topped" hexes, like so:
+    ____
+   /    \
+  /      \
+  \      /
+   \____/
+
+HexCoord's are also Ix.  An array using HexCoords
+as Ix will be a rectangular section:
+     ____        ____
+    /    \      /    \
+   / lower\____/      \
+   \ bound/    \      /
+    \____/      \____/
+    /    \      /    \
+   /      \____/      \
+   \      /    \      /
+    \____/      \____/
+    /    \      /    \
+   /      \____/ upper\
+   \      /    \ bound/
+    \____/      \____/
+         \      /
+          \____/
+
+-}
+
+module Merch.Race.Hex
+  ( HexCoord(..)
+  , neighbors
+  , nearby
+  , distance
+
+  , toOffset
+  , fromOffset
+  ) where
+
+import Data.Array
+
+newtype HexCoord
+  = HexCoord (Int, Int)
+  deriving (Show, Read, Eq)
+
+toCubic :: HexCoord -> (Int, Int, Int)
+toCubic (HexCoord (x,z)) = (x, negate (x + z), z)
+fromCubic :: (Int, Int, Int) -> HexCoord
+fromCubic (x, _, z) = HexCoord (x,z)
+
+-- odd-q offset
+toOffset :: HexCoord -> (Int, Int)
+toOffset (HexCoord (x,z)) = (q, r)
+ where
+  q = x
+  r = z + ((x - (oddv q)) `div` 2)
+fromOffset :: (Int, Int) -> HexCoord
+fromOffset (q, r) = HexCoord (x, z)
+ where
+  x = q
+  z = r - ((q - (oddv q)) `div` 2)
+
+oddv :: Int -> Int
+oddv i
+  | odd i     = 1
+  | otherwise = 0
+
+instance Ord HexCoord where
+  compare h1 h2 =
+    case compare r1 r2 of
+      EQ  -> compare q1 q2
+      oth -> oth
+   where
+    (q1, r1) = toOffset h1
+    (q2, r2) = toOffset h2
+instance Ix HexCoord where
+  range (l, h) = map fromOffset $ range (toOffset l, toOffset h)
+  index (l, h) i = index (toOffset l, toOffset h) (toOffset i)
+  inRange (l, h) i = inRange (toOffset l, toOffset h) (toOffset i)
+
+{- Returns a list of hex coordinates directly
+   adjacent to the given hex.  -}
+neighbors :: HexCoord -> [HexCoord]
+neighbors (HexCoord (x,z)) =
+  [ HexCoord (xp1, z  )
+  , HexCoord (xp1, zm1)
+  , HexCoord (x  , zm1)
+  , HexCoord (xm1, z  )
+  , HexCoord (xm1, zp1)
+  , HexCoord (x  , zp1)
+  ]
+ where
+  xp1 = x + 1
+  xm1 = x - 1
+  zp1 = z + 1
+  zm1 = z - 1
+
+{- Returns a list of hex coordinates within n
+   hexes of the given hex, including the given
+   hex.  -}
+nearby :: Integral i => i -> HexCoord -> [HexCoord]
+nearby ni (HexCoord (x,z)) = do
+  let n = fromIntegral ni
+  deltaX <- [negate n..n]
+  deltaY <- [max(negate n)(negate deltaX-n)..min(n)(negate deltaX+n)]
+  let deltaZ = negate (deltaX + deltaY)
+  return $ HexCoord (x + deltaX, z + deltaZ)
+
+{- Determines the distance between two hexes.  -}
+distance :: HexCoord -> HexCoord -> Int
+distance (HexCoord (x1, z1)) (HexCoord (x2, z2)) =
+  (abs(x1 - x2) + abs(z1 - z2) +
+    abs(x1 + z1 - x2 - z2)) `div` 2
