36f69e1d7cbbb6f16ad3f19d414fc6d316452f0c
Alan Manuel K. Gloria <almkglor@gmail.com>
2013-11-23 14:44:45 +0800

    Main: Start filling in code.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,47 @@
 
 module Main(main) where
 
-main = return ()
+import qualified Merch.Race.Ruleset as Ruleset
+import Merch.Race.Ruleset(Ruleset)
+import Merch.Race.Ruleset.Load
+
+import Paths_merchrace
+
+import System.Exit
+import System.IO.Error
+
+-- attempt to load the ruleset.
+tryLoadRuleset :: IO (Either String Ruleset)
+tryLoadRuleset = do
+  dir <- getDataFileName "ruleset"
+  catch (loadRuleset dir >>= return . Right)
+        (return . Left . show)
+
+core :: IO ()
+core = do
+  ers <- tryLoadRuleset
+  case ers of
+    Left e  -> reportError e
+    Right r -> gameLoop r
+
+-- Report an error loading ruleset
+reportError :: String -> IO ()
+reportError e = do
+  -- TODO: Display the error in the GUI
+  putStrLn "Error loading ruleset"
+  putStrLn e
+
+  exitWith $ ExitFailure 1
+
+-- Main game loop
+gameLoop :: Ruleset -> IO ()
+gameLoop r = do
+  -- TODO: Actually operate the game
+  putStrLn "Merchant's Race!"
+  return ()
+
+-- Initialize the graphics system
+beginGraphics :: IO () -> IO ()
+beginGraphics = id -- TODO
+
+main = beginGraphics core
