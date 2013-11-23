9b578ccae3077dd3877fd7c84131fb4a2d486daa
Alan Manuel K. Gloria <almkglor@gmail.com>
2013-11-24 00:20:56 +0800

    Merch.Race.Graphics: Remove Merch.Race.Graphics.Init, integrate callback system.

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 
 module Main(main) where
 
-import Merch.Race.Graphics.Init
+import Merch.Race.Graphics
 import qualified Merch.Race.Ruleset as Ruleset
 import Merch.Race.Ruleset(Ruleset)
 import Merch.Race.Ruleset.Load
@@ -36,7 +36,7 @@ core = do
 -- Report an error loading ruleset
 reportError :: Draw.Font -> String -> IO ()
 reportError font e = do
-  -- TODO: Display the error in the GUI
+  -- Create the error message image.
   let elines = lines e
       rawtexts = map (Draw.text font) elines
       texts = map translate $ zip rawtexts [0..]
@@ -45,13 +45,12 @@ reportError font e = do
       textwidths = map (Draw.textWidth font) elines
       textwidth = foldl' max 0.1 textwidths
       text = foldl' mappend mempty texts
-      scaledtext = Draw.scale (1/textwidth) (1/textwidth) %% text
-      top = Draw.translate (-0.5, 0) %% scaledtext
-      display = do
-        GLUT.clear [GLUT.ColorBuffer]
+      scaledtext = Draw.scale (2/textwidth) (2/textwidth) %% text
+      top = Draw.translate (-1, 0) %% scaledtext
+  -- Display the error message.
+  let display aspect = do
         Draw.render top
-        GLUT.flush
-  GLUT.displayCallback $= display
+  displayFunc $= display
   GLUT.mainLoop
   exitWith $ ExitFailure 1
 
@@ -60,11 +59,9 @@ gameLoop :: Draw.Font -> Ruleset -> IO ()
 gameLoop font r = do
   -- TODO: Setup callbacks
   let top = Draw.text font "Merchant's Race"
-      display = do
-        GLUT.clear [GLUT.ColorBuffer]
+      display aspect = do
         Draw.render top
-        GLUT.flush
-  GLUT.displayCallback $= display
+  displayFunc $= display
   GLUT.mainLoop
   return ()
 
