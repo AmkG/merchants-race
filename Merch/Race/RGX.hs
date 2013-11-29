ba0e21de54e851e34c31711183c3fd618291b7b3
Alan Manuel K. Gloria <almkglor@gmail.com>
2013-11-29 19:51:45 +0800

    Merch.Race.RGX: Random number generator.
      Needed so we can pass around a random number generator
        that we can also store in the savegame file.

---
+++ Merch/Race/RGX.hs
@@ -0,0 +1,62 @@
+
+module Merch.Race.RGX
+  ( RGX
+  , mkRGX
+  , feedRGX
+  ) where
+
+import Merch.Race.Data.Serialize
+
+import Data.Bits
+import Data.Word
+import System.Random
+
+data RGX
+  = RGX !Word32 !Word32 !Word32 !Word32
+  deriving (Show, Read)
+
+-- Paper "Xorshift RNGs", by Marsaglia, George
+get :: RGX -> (Word32, RGX)
+get (RGX x y z w) = (rv, RGX y z w rv)
+ where
+  tmp = x `xor` (x `shiftL` 15)
+  rv = (w `xor` (w `shiftR` 21)) `xor` (tmp `xor` (tmp `shiftR` 4))
+
+instance RandomGen RGX where
+  genRange _ = (minBound, maxBound)
+  next g = let (w, g') = get g in (fromIntegral w, g')
+  split g = (RGX x y z w, g3)
+   where
+    (x, g0) = get g
+    (y, g1) = get g0
+    (z, g2) = get g1
+    (w, g3) = get g2
+-- RGX can also be generated from other generators.
+instance Random RGX where
+  -- range makes no sense for RGX.
+  randomR _ = random
+  random g = (RGX x y z w, g3)
+   where
+    (ix, g0) = next g
+    (iy, g1) = next g0
+    (iz, g2) = next g1
+    (iw, g3) = next g2
+    x = fromIntegral ix
+    y = fromIntegral iy
+    z = fromIntegral iz
+    w = fromIntegral iw
+instance Serialize RGX where
+  hPut = hPutConvert $ \ (RGX x y z w) -> (x, y, z, w)
+  hGet = hGetConvert $ \ (x, y, z, w)  -> (RGX x y z w)
+ 
+mkRGX :: Int -> RGX
+mkRGX i = RGX ri (ri `xor` 1) (ri `xor` 2) (ri `xor` 4)
+ where
+  ri = fromIntegral i
+feedRGX :: Show a => a -> RGX -> RGX
+feedRGX a = feed (show a)
+
+feed :: String -> RGX -> RGX
+feed []     g = g
+feed (c:cs) g = let (_, RGX x y z w) = get g
+                in feed cs $ RGX x y z (w `xor` (fromIntegral $ fromEnum c))
